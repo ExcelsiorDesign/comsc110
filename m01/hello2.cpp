9d6a4b6a996b2953e78c91a33456ca8ede9fae5a
Tony Chern <run1@aslugs.com>
2018-09-04 18:39:08 +0000

    090418

---
+++ m01/hello2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int ivar;
+    cout << "enter an integer: ";
+    cin >> ivar ;
+    cout << "Hello world!" << endl;
+    cout << "You have entered " << ivar;
+}
