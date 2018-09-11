82db488bf2a302a531c2d1f01ebfa2af1fc18d1e
Tony Chern <run1@aslugs.com>
2018-09-11 19:02:37 +0000

    091118

--- m01/m01_review.cpp
+++ m01/m01_review.cpp
@@ -1,17 +1,21 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 int main() {
     int i, j=2;
-    double d, f=3.4;
+    double d, f=3.4567;
     string str="hello, you have entered: ";
+    
     cout << "enter an integer: ";
     cin >> i;
     cout << "enter another float: ";
     cin >> d;
     cout << str << i << ", " << d;
-    cout << "\nInternal numbers are: "
-         << j << ", " << f;
+    cout << "\nInternal numbers are: " // wiki Escape_sequences_in_C
+         << j << ", " << f << ", "
+         << fixed << setprecision(2) << f ;
+
     
     
 }
