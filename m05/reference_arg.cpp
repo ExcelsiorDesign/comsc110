3faf87b5bb0b28b73e13238f9b088941e54c3b7f
Tony Chern <run1@aslugs.com>
2018-10-09 18:55:16 +0000

    100918

--- m05/reference_arg.cpp
+++ m05/reference_arg.cpp
@@ -3,11 +3,14 @@ using namespace std;
 
 void f(int &a, int &b) {
     b = 2*a +1;
+    cout << "internal: " << a << ", " << b << endl;
 }
 
 int main() {
     int x = 1, y=2;
     
+    f(x, y);
+    cout << x << " " << y << endl;
     f(x, y);
     cout << x << " " << y << endl;
 }
